d2aa307aa620993ce9b40366986139968d841201
Axel-Egide Hounsou <egide95@gmail.com>
2019-07-27 11:36:03 -0400

    added a profile page

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -31,7 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my apps
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    # installed for forms
+    'crispy_forms',
+
+    #
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
