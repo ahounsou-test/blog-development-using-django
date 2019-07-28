98d35771f1d2bfe1ec17121f74db31b8d95388c3
Axel-Egide Hounsou <egide95@gmail.com>
2019-07-27 23:48:08 -0400

    added venv

--- blog_project/users/models.py
+++ blog_project/users/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 class  Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Bio = models.TextField()
+    bio = models.TextField()
     image = models.ImageField(default='default.jpg' , upload_to='profile_pics')
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+    # def __str__(self):
+    #     return f'{self.user.username} Profile'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/axel/Desktop/my_django_project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
