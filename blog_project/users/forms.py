fafc9f201dc3b5df337318c8818791a53ef78acc
Axel-Egide Hounsou <egide@gmailcom>
2019-07-28 22:00:57 -0400

    added a form to the user profile

--- blog_project/users/forms.py
+++ blog_project/users/forms.py
@@ -1,15 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        #create a new user when form validate
+        # create a new user when form validate
         model = User
 
         # field to show on the forms
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        # create a new user when form validate
+        model = User
+
+        # field to show on the forms
+        fields = ['username', 'email',]
+
+
+class ProfileUpdateFrom(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
